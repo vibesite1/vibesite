b3f7d7641b12007b68dab65f65bc4f1dbbd0c517
codoknam <benjiekim14@gmail.com>
2026-05-17 08:19:34 +0900

    Set up Express server with POST endpoint

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+const express=require("express");
 
+const app=express();
+
+app.use(express.json());
+app.use(express.static("./"));
+
+app.post("/generate",async(req,res)=>{
+
+const prompt=req.body.prompt;
+
+res.json({
+code:
+`<!DOCTYPE html>
+<html>
+<body>
+<h1>${prompt}</h1>
+</body>
+</html>`
+});
+
+});
+
+app.listen(
+process.env.PORT||3000
+);
